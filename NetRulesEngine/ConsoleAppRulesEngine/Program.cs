bf2e3c440756f26ad9afa052d6b6e17e02b64a1b
Daniel Fernandes <daniel.fernandes@sbcredito.com.br>
2021-03-16 08:11:13 -0300

    First commit with git ignore

---
+++ NetRulesEngine/ConsoleAppRulesEngine/Program.cs
@@ -0,0 +1,29 @@
+﻿using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Dynamic;
+
+namespace ConsoleAppRulesEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regras = File.ReadAllText("Regras.json");
+
+            var workflowRules = new List<WorkflowRules>()
+            {
+                new WorkflowRules()
+            };
+
+            var expandoObject = new ExpandoObjectConverter();
+            var exp = new ExpandoObject();
+
+            var rulesEngine = new RulesEngine.RulesEngine();
+        }
+    }
+}
